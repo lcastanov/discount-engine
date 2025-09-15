badaa722d4843fe9df74dcbb0a855db05304bae2
Laura <lauracastanovilla@usf.edu>
2025-09-15 15:53:16 -0400

    initial products

--- cc_4.js
+++ cc_4.js
@@ -4,5 +4,5 @@ let products = [
     {name: "iPad Air", category: "Electronics", price: 399.99, inventory: 200},
     {name: "Crop Top", category: "Clothing", price: 19.72, inventory: 30},
     {name: "Ruled Notebook", category: "School Supplies", price: 3.99, inventory: 50},
-    {name: "Red Lipstick", category: "Makeup", price: 8.50, inventory: 32}
+    {name: "Red Lipstick", category: "Makeup", price: 8.50, inventory: 15}
 ]
