343d52035dc45e45205663bb7d4529f015763a87
Laura <lauracastanovilla@usf.edu>
2025-09-21 11:40:49 -0400

    added for... in switch

--- cc_4.js
+++ cc_4.js
@@ -2,7 +2,25 @@
 
 let products = [
     {name: "iPad Air", category: "Electronics", price: 399.99, inventory: 200},
-    {name: "Crop Top", category: "Clothing", price: 19.72, inventory: 30},
-    {name: "Ruled Notebook", category: "School Supplies", price: 3.99, inventory: 50},
-    {name: "Red Lipstick", category: "Makeup", price: 8.50, inventory: 15}
+    {name: "Crop Top", category: "Apparel", price: 19.72, inventory: 30},
+    {name: "Avocados", category: "Groceries", price: 3.99, inventory: 50},
+    {name: "Red Leggings", category: "Apparel", price: 8.50, inventory: 15}
 ]
+
+for (let products in products) {
+    let discount
+    
+    switch (products.category) {
+        case 'Electronics': 
+            discount = .2
+            break;
+        case 'Groceries':
+            discount = .10
+            break;
+        case 'Apparel':
+            discount = .15
+            break;
+        default:
+            discount = 0
+    }
+}
